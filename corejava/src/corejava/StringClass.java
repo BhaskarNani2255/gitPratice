9a30fdbb6a171f0f218e3b3b7d20c3cb7e29301b
bhask <bhask@LAPTOP-U33I03NT>
2024-02-26 12:45:26 +0530

    1

---
+++ corejava/src/corejava/StringClass.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class StringClass 
+{
+	public static void main(String[] args)
+	{
+		String s1="jhansirani";  //literal way  of creating a string
+		String s2="jhansi";
+		String s3="92";
+		int i=Integer.valueOf(s3);
+		System.out.println(i);
+		
+		System.out.println(s1==s3);
+		
+		
+	}
+}
+ 
